2357f8d560fcea08bd9ce325e80596d4057e56ae
Otieno Felix Ongalo <98937311+FelixOngalo@users.noreply.github.com>
2023-04-05 03:13:13 +0300

    Create AuthenticationConnection.py
    Code to authenticate and connect to the Twitter API using the tweepy library.
    
    
    Replace YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET, YOUR_ACCESS_TOKEN, and YOUR_ACCESS_TOKEN_SECRET with the corresponding API credentials that you obtained from Twitter. The code uses the OAuthHandler class to authenticate with the Twitter API and the API class to create an API object for accessing the API. Finally, the code verifies the API credentials by calling the verify_credentials() method.

---
+++ AuthenticationConnection.py
@@ -0,0 +1,22 @@
+import tweepy
+
+# API credentials
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Test API connection
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except Exception as e:
+    print("Error during authentication")
+    print(e)
